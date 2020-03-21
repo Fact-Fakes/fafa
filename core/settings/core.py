ef036c8145924fbe12050d37823f678a4d98e752
Daniel Różycki <daniel.rozycki@stxnext.pl>
2020-03-21 10:01:27 +0100

    CORS hotfix

--- core/settings/core.py
+++ core/settings/core.py
@@ -13,6 +13,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "backend"
 ]
 
@@ -84,3 +85,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
