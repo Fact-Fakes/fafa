82aadc190fb099c6336712c8aba1a4701b86b9a9
lukortech <lukortech@wp.pl>
2020-03-22 11:39:07 +0100

    Infinite scroll and minor changes

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import InfiniteScroll from "react-infinite-scroller";
+import { Question } from "../../components";
+import { getQuestions, QuestionProps } from "../../requests/AxiosRequest";
+import Cookies from "js-cookie";
 
 const App: React.FC = () => {
   const { t } = useTranslation();
+  const initialQuestion = {
+    pk: 0,
+    title: "",
+    is_true: true,
+    real_answer: "",
+    yes_answers: 0,
+    no_answers: 0,
+    up_votes: 0,
+    down_votes: 0,
+    keywords: [""],
+    answers: null,
+    votes: [],
+    attachments: [],
+    experts: []
+  };
+  const [questions, setQuestions] = useState<QuestionProps[]>([]);
+  const cookieSessionID = Cookies.get("sessionId");
+
+  const getQuestionsWrapper = async (page: any) => {
+    const fetchedQuestions = await getQuestions(
+      `questions${page ? `/?page=${page}&sessionID=${cookieSessionID}` : ""}`
+    );
+    console.log(fetchedQuestions);
+    setQuestions(prevState => {
+      const newState = prevState.concat(fetchedQuestions);
+      return newState;
+    });
+  };
 
   return (
     <div className="App">
       <div className="container">
         <div className="row">
-          <div className="col-12 d-flex flex-column text-center">
+          <div className="col-12 d-flex flex-column text-center mb-5">
             <span className="text-white">
               {t("Weryfikujemy newsy o koronawirusie")}
             </span>
@@ -17,14 +48,21 @@ const App: React.FC = () => {
               {t("Sprawdzaj swoją wiedzę i bądź na bieżąco")}
             </span>
           </div>
-          {/* <InfiniteScroll
-        pageStart={0}
-        loadMore={getQuestions(`/?page=${page}&sessionID=${cookieSessionID}`);
-    hasMore={true || false}
-    loader={<div className="loader" key={0}>Loading ...</div>}
->
-    {items} // <-- This is the content you want to load
-</InfiniteScroll> */}
+          <InfiniteScroll
+            pageStart={1}
+            loadMore={async page => await getQuestionsWrapper(page)}
+            hasMore={true || false}
+            initialLoad={true}
+            loader={
+              <div className="loader" key={0}>
+                Loading ...
+              </div>
+            }
+          >
+            {questions.map(question => {
+              return <Question question={question} />;
+            })}
+          </InfiniteScroll>
         </div>
       </div>
     </div>
