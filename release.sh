8719b659689c82ed488dacda64220f475aa11e04
AlTosterino <altosterino@gmail.com>
2020-04-26 13:03:12 +0200

    More fixes

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
-sudo apt install libpq-dev
 pip install pipenv
 pipenv install
-pipenv install psycopg2
 pipenv install gunicorn
 
-pipenv run python manage.py flush --no-input --settings=core.settings.production
-pipenv run python manage.py migrate --settings=core.settings.production
-pipenv run python manage.py load_excel --settings=core.settings.production
+python manage.py flush --no-input --settings=core.settings.production
+python manage.py migrate --settings=core.settings.production
+python manage.py load_excel --settings=core.settings.production
