0066a40dd699e972d940d66aded7a852d19c6eb0
AlTosterino <altosterino@gmail.com>
2020-04-26 14:38:08 +0200

    Changes for frontend

--- core/settings/production.py
+++ core/settings/production.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "fafaapp.herokuapp.com"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "fafaapi.herokuapp.com"]
 
 
 # Database
