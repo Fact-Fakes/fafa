f77dff763e9b4032a52cb0b3176eaf5e4ecb5cd8
AlTosterino <altosterino@gmail.com>
2020-04-26 13:28:19 +0200

    Fixes...

--- release.sh
+++ release.sh
@@ -1,9 +1,5 @@
-pip install --upgrade pip
-pip install --upgrade pipenv
-pipenv install
 pipenv install gunicorn
-pip install gunicorn
 
-pipenv run python manage.py flush --no-input --settings=core.settings.production
-pipenv run python manage.py migrate --settings=core.settings.production
-pipenv run python manage.py load_excel --settings=core.settings.production
+python manage.py flush --no-input --settings=core.settings.production
+python manage.py migrate --settings=core.settings.production
+python manage.py load_excel --settings=core.settings.production
