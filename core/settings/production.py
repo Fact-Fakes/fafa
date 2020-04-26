07552fe37de0051a84db86cb5e76759131bfa5c7
AlTosterino <altosterino@gmail.com>
2020-04-26 13:48:34 +0200

    Add whitenoise

--- core/settings/production.py
+++ core/settings/production.py
@@ -36,5 +36,6 @@ DATABASES = {
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
