18dc86b815fb7ada340d232d3df15e0e631488ed
AlTosterino <altosterino@gmail.com>
2020-04-26 13:22:42 +0200

    Fixes...

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
-pip install pipenv
+pip install --upgrade pip
+pip install --upgrade pipenv
 pipenv install
 pipenv install gunicorn
+pip install gunicorn
 
-python manage.py flush --no-input --settings=core.settings.production
-python manage.py migrate --settings=core.settings.production
-python manage.py load_excel --settings=core.settings.production
+pipenv run python manage.py flush --no-input --settings=core.settings.production
+pipenv run python manage.py migrate --settings=core.settings.production
+pipenv run python manage.py load_excel --settings=core.settings.production
